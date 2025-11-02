0c4e483b6dbe26afe6984111c20dc7f57a0308f1
ThisIsNoZaku <damienmarble@gmail.com>
2025-11-01 23:38:05 -0700

    Test for the specification of tictactoe.

---
+++ test/examples/tictactoe/GameSpec.spec.ts
@@ -0,0 +1,39 @@
+import tictactoe from "../../../examples/tictactoe.schema.json";
+import {FeatureSpec} from "../../../src/specification/ModelSpecs";
+import {Game} from "../../../src/rules/Game";
+
+describe("Tictactoe spec", () => {
+    it("defines the specification for encoding", async () => {
+        const [game] = Game.GenerateFromDefinition(tictactoe);
+        expect(await game.spec()).toEqual({
+            fields: [
+                {
+                    key: "activePlayer",
+                    kind: "numeric"
+                },
+                {
+                    key: "players",
+                    kind: "list",
+                    length: 2,
+                    contains: "dict",
+                    shape: []
+                },
+                {
+                    key: "locations",
+                    kind: "list",
+                    contains: "dict",
+                    length: 11,
+                    shape: [
+                        {
+                            key: "id",
+                            kind: "numeric",
+                        },
+                        {
+                            key: "value",
+                            kind: "numeric"
+                        }],
+                }
+            ]
+        } as FeatureSpec);
+    });
+})
