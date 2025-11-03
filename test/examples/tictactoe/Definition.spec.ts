03197ee1f0d0853d876de693391e45ac86e72103
ThisIsNoZaku <damienmarble@gmail.com>
2025-11-02 16:04:35 -0800

    Removed namespace.

--- test/examples/tictactoe/Definition.spec.ts
+++ test/examples/tictactoe/Definition.spec.ts
@@ -2,43 +2,41 @@ import tictactoe from "../../../examples/tictactoe.schema.json";
 import {RuleDocumentValidator} from "../../../src/json/RuleDocumentValidator";
 import {Game} from "../../../src/rules/Game";
 
-namespace Tictactoe {
-    describe("Tictactoe Definition", () => {
-        it("loads and validates the definition", () => {
-            const output: any = new RuleDocumentValidator().validate(tictactoe);
-            expect(output).toBeDefined();
-            expect(output.metadata.id).toBe("tictactoe");
-            expect(output.agents).toEqual({
-                exactly: 2
-            });
-            expect(output.locations).toEqual({
-                "board": {
-                    "shape": {
-                        "cols": 3,
-                        "rows": 3,
-                        "state": {
-                            "value": {
-                                "type": "integer",
-                                "min": 0,
-                                "max": 2
-                            }
-                        }
-                    }
-                }
-            });
-            expect(output.actions).toEqual([
-                {
-                    id: "mark",
-                    parameters: {
-                        location: {
-                            location: {
-                                where: "value === 0"
-                            }
-                        }
-                    }
-                }
-            ]);
-            expect(output.rules).toBeDefined();
+describe("Tictactoe Definition", () => {
+    it("loads and validates the definition", () => {
+        const output: any = new RuleDocumentValidator().validate(tictactoe);
+        expect(output).toBeDefined();
+        expect(output.metadata.id).toBe("tictactoe");
+        expect(output.agents).toEqual({
+            exactly: 2
         });
-    })
-}
+        expect(output.locations).toEqual({
+            "board": {
+                "shape": {
+                    "cols": 3,
+                    "rows": 3,
+                    "state": {
+                        "value": {
+                            "type": "integer",
+                            "min": 0,
+                            "max": 2
+                        }
+                    }
+                }
+            }
+        });
+        expect(output.actions).toEqual([
+            {
+                id: "mark",
+                parameters: {
+                    location: {
+                        location: {
+                            where: "value === 0"
+                        }
+                    }
+                }
+            }
+        ]);
+        expect(output.rules).toBeDefined();
+    });
+})
