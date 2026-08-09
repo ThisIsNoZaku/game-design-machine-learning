216ed18faff8a858d845535ab6c74d4d79ae29e3
ThisIsNoZaku <damienmarble@gmail.com>
2026-08-09 18:48:12 -0400

    Implemented initial state and action.

--- examples/tictactoe.ts
+++ examples/tictactoe.ts
@@ -1,13 +1,10 @@
-import {GameDescriptionExpansion} from "../src/expansion";
 import {
     Agents,
-    BaseGameDefinition,
     GameDefinition,
     GameDefinitionMetadata
 } from "../src/definitions/BaseGameDefinition";
 import {Region} from "../src/definitions/Region";
 import {ActionDefinition} from "../src/definitions/ActionDefinition";
-import metadata from "ajv/lib/vocabularies/jtd/metadata";
 import {GameState} from "../src/state";
 
 export class Tictactoe implements GameDefinition {
@@ -82,13 +79,72 @@ export class Tictactoe implements GameDefinition {
                 active: "1"
             },
             entities: {},
-            regions: {},
-            terminated: false
+            regions: Object.fromEntries(Object.entries(this.locations).map(([id, location]) => [id, {
+                ...location,
+                state: {
+                    value: 0
+                }
+            }])),
+            terminated: false,
+            winners: []
         }
     }
 
     get actions(): Array<ActionDefinition> {
-        return undefined;
+        return [
+            {
+                id: "mark",
+                parameters: {
+                    "location" : "string"
+                },
+                execute: (game, state, parameters) => {
+                    const locationId = parameters["location"] as string;
+                    const actorId = state.players.active;
+
+                    const region = state.regions[locationId];
+                    if (!region) {
+                        throw new Error(`Location '${locationId}' does not exist`);
+                    }
+                    if (region.state["value"]) {
+                        throw new Error(`Location '${locationId}' is already marked`);
+                    }
+
+                    region.state["value"] = parseInt(actorId);
+
+                    const winLines = [
+                        ["0_0", "0_1", "0_2"],
+                        ["1_0", "1_1", "1_2"],
+                        ["2_0", "2_1", "2_2"],
+                        ["0_0", "1_0", "2_0"],
+                        ["0_1", "1_1", "2_1"],
+                        ["0_2", "1_2", "2_2"],
+                        ["0_0", "1_1", "2_2"],
+                        ["0_2", "1_1", "2_0"],
+                    ];
+
+                    const playerValue = parseInt(actorId);
+                    const hasWon = winLines.some(line =>
+                        line.every(id => state.regions[id]?.state["value"] === playerValue)
+                    );
+
+                    if (hasWon) {
+                        state.terminated = true;
+                        state.winners = [state.players.active];
+                        return;
+                    }
+
+                    const cellIds = ["0_0", "0_1", "0_2", "1_0", "1_1", "1_2", "2_0", "2_1", "2_2"];
+                    const isDraw = cellIds.every(id => state.regions[id]?.state["value"] !== 0);
+                    if (isDraw) {
+                        state.terminated = true;
+                        state.winners = [];
+                        return;
+                    }
+
+                    state.players.active = actorId === "1" ? "2" : "1";
+                }
+            }
+        ];
     }
 
 }
