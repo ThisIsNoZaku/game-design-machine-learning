0d948fb9d1763fad37ebd57e7bdf7b34ccda5edc
ThisIsNoZaku <damienmarble@gmail.com>
2026-08-09 21:49:10 -0400

    Updated export.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
     presets: [
         "@babel/preset-env", // Transpile modern JavaScript to older versions for compatibility
         "@babel/preset-typescript" // Add support for TypeScript
