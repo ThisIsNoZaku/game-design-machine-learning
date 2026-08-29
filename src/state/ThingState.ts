b6056b411de1c161ba716ae7b8bf24a0553549e4
ThisIsNoZaku <damienmarble@gmail.com>
2026-08-29 16:31:38 -0400

    Remove id from state.

--- src/state/ThingState.ts
+++ src/state/ThingState.ts
@@ -1,6 +1,7 @@
 /**
  * Runtime state of anything that exists within the game: players, entities, locations, etc.
  */
+// TODO: Turn this into a dynamic type, where different things can have different state shapes, and the shape is defined by the game definition.
 export interface ThingState {
-    id:  string;
+
 }
